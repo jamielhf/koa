3701f34698a3d9a2788bf4075fd4ea2794f19224
linhaifeng <linhf@yunyichina.cn>
2017-10-24 17:59:51 +0800

    更新

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const Koa = require('koa')
 const app = new Koa()
 const fs = require('fs');
-const bodyParser = require('koa-bodyparser')
-const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')   //解析post数据的中间件
+const Router = require('koa-router') //路由中间件
+
+const path = require('path')
+const serve = require('koa-static'); //处理静态文件中间件
+
+const views = require('koa-views')
+const { query } = require('./lib/conf')
+
+
+async function selectAllData( ) {
+    let sql = 'SELECT * FROM user'
+    let dataList = await query( sql )
+    return dataList
+}
+
+async function getData() {
+    let dataList = await selectAllData()
+    console.log( dataList )
+}
+
+getData()
+
+
+// 加载模板引擎
+app.use(views(path.join(__dirname, './views'), {
+    extension: 'ejs'
+}))
+
+const main = serve(path.join(__dirname,'./static'));
+
+app.use(main);
+
+
 
 // 使用ctx.body解析中间件
 app.use(bodyParser())
@@ -11,15 +43,34 @@ let router = new Router()
 
 
 
+router.get('/api/getApi', async function (ctx, next) {
+    let result = {
+        success:true,
+        data:{
+            time:new Date()
+        }
+    }
+    ctx.body  = result
+});
 
 
 
-router.get('/list', async (ctx)=>{
+router.get('/', async function (ctx, next) {
+    let title = 'jamie';
+    await ctx.render('index', {
+        title,
+    })
+});
 
+router.post('/', function (ctx, next) {
+    let postData = ctx.request.body
+    ctx.body = postData
+});
+router.get('/list', async function (ctx, next) {
 
+    await ctx.render('list')
 
-})
-
+});
 
 
 app.use(router.routes())
---
+++ util/db.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql')
+
+const pool = mysql.createPool({
+    host     :  '127.0.0.1',
+    user     :  'root',
+    password :  '123456',
+    database :  'koa_demo'
+})
+
+let query = function( sql, values ) {
+
+    return new Promise(( resolve, reject ) => {
+        pool.getConnection(function(err, connection) {
+            if (err) {
+                reject( err )
+            } else {
+                connection.query(sql, values, ( err, rows) => {
+
+                    if ( err ) {
+                        reject( err )
+                    } else {
+                        resolve( rows )
+                    }
+                    connection.release()
+                })
+            }
+        })
+    })
+
+}
+
+module.exports = {
+    query
+}
