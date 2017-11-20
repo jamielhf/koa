cac74b01c045bcc48022edc4017f3f250f79a731
linhaifeng <linhf@yunyichina.cn>
2017-11-20 18:19:39 +0800

    更新目录

---
+++ static/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+
---
+++ static/src/page/IndexPage.vue
@@ -0,0 +1,47 @@
+<template>
+   <div class="g-index">
+      <Header></Header>
+      <Footer></Footer>
+   </div>
+</template>
+
+<script>
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    Header,
+    Footer
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
